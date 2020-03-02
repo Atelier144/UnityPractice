950601be426c1ac2d05c3319cad5fd2c3e06618f
Atelier144 <amestric16@gmail.com>
2020-03-03 08:40:59 +0900

    created unirx

---
+++ Assets/Scripts/UniRxSceneManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class UniRxSceneManager : MonoBehaviour
+{
+    ReactiveProperty<int> _count = new ReactiveProperty<int>(5);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _count.Subscribe((count) =>
+        {
+            Debug.Log(count);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _count.Value++;
+        
+    }
+}
